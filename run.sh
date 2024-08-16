785806147216a705a6ca7b23bcbb76167b386c55
JustAnyone <dom.svetikas@gmail.com>
2024-08-17 00:45:58 +0300

    Bundle python 2.7, move the files to XDG_DATA_HOME, include default settings file with binary paths pre set

--- run.sh
+++ run.sh
@@ -4,6 +4,12 @@ opts=(
   -Dswing.crossplatformlaf='com.sun.java.swing.plaf.gtk.GTKLookAndFeel'
   -Dawt.useSystemAAFontSettings='on'
   -Dswing.aatext='true'
+  -Duser.home=$XDG_DATA_HOME # ugly hack for BCV 2.12 to save in flatpak data dir
 )
+
+# Install default settings file with the binary paths set
+if ! [ -f "$XDG_DATA_HOME/.Bytecode-Viewer/settings.bcv" ]; then
+  cp "/app/settings.bcv" "$XDG_DATA_HOME/.Bytecode-Viewer/settings.bcv"
+fi
 
 exec /app/jre/bin/java "${opts[@]}" -jar '/app/bin/bytecodeviewer.jar' "$@"
